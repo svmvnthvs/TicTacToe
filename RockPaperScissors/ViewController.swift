dc7d4a0317f53579d3a7e2543989ccd0809a8b00
svmvnthvs <samanthaleung@alumni.ubc.ca>
2019-05-08 11:08:01 -0700

    Initial Commit

---
+++ RockPaperScissors/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RockPaperScissors
+//
+//  Created by Samantha Leung on 2019-05-08.
+//  Copyright © 2019 Samantha Leung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
